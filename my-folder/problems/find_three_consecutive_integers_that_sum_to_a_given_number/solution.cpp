fac3686b1677f9be0ddfea89386641a5051d1f9b
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-02-19 14:38:47 +0000

    Sync LeetCode submission - Find Three Consecutive Integers That Sum to a Given Number (cpp)

---
+++ my-folder/problems/find_three_consecutive_integers_that_sum_to_a_given_number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        vector<long long> v;
+        if(num%3 != 0)
+            return v;
+        long long x = num/3 -1;
+        v.push_back(x);
+        v.push_back(x+1);
+        v.push_back(x+2);
+        return v;
+        
+    }
+};
