61ca93eb045a71cadbb16f971bb430bd94e8034c
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-02-22 16:56:28 +0000

    Sync LeetCode submission - Pascal's Triangle II (cpp)

---
+++ my-folder/problems/pascal's_triangle_ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> v;
+        vector<int> x(1,1);
+        v.push_back(x);
+        for(int i = 0; i < 33; i++)
+        {
+            vector<int> temp;
+            temp.push_back(v[0][0]);
+            for(int j = 0; j < v[i].size()-1; j++)
+            {
+                int num = v[i][j] + v[i][j+1];
+                temp.push_back(num);
+            }
+            int size = v[i].size()-1;
+            temp.push_back(v[i][size]);
+            v.push_back(temp);
+        }
+        return v[rowIndex];
+    }
+};
