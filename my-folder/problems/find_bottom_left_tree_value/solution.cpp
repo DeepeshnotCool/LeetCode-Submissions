53228366642c68e3256a9d82586a908bf3135be1
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-02-25 01:36:00 +0000

    Sync LeetCode submission - Find Bottom Left Tree Value (cpp)

---
+++ my-folder/problems/find_bottom_left_tree_value/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, int level, vector<int>& v)
+    {
+        if(root == NULL)
+            return;
+        if(v.size() == level)
+            v.push_back(root -> val);
+        helper(root -> left, level + 1, v);
+        helper(root -> right, level + 1, v);
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        vector<int> v;
+        helper(root, 0, v);
+        return v[v.size()-1];
+    }
+};
