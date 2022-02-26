a0e5bad8b5f6517b01520533ad495f271896f806
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-02-26 16:42:50 +0000

    Sync LeetCode submission - Increasing Order Search Tree (cpp)

---
+++ my-folder/problems/increasing_order_search_tree/solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* helper(vector<int> &v)
+    {
+        if(v.size() == 0)
+            return NULL;
+        TreeNode* root = new TreeNode(v[0]);
+        TreeNode* temp = root;
+        for(int i = 1; i < v.size(); i++)
+        {
+            TreeNode* node = new TreeNode(v[i]);
+            temp -> right = node;
+            temp = temp -> right;
+        }
+        return root;
+        
+    }
+    
+    void inorder(TreeNode* root, vector<int> &v)
+    {
+        if(root == NULL)
+            return;
+        inorder(root -> left, v);
+        v.push_back(root -> val);
+        inorder(root -> right, v);
+    }
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int> v;
+        inorder(root, v);
+        return helper(v);
+    }
+};
