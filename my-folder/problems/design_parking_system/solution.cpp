4a6f932b68cb3b99c4e6cf964f2c610611515f72
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-03-07 19:40:09 +0000

    Sync LeetCode submission - Design Parking System (cpp)

---
+++ my-folder/problems/design_parking_system/solution.cpp
@@ -0,0 +1,52 @@
+class ParkingSystem {
+    vector<int> v;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        v.push_back(big);
+        v.push_back(medium);
+        v.push_back(small);
+    }
+    
+    bool addCar(int carType) {
+        if(carType == 1)
+        {
+            if(v[0] != 0)
+            {
+                v[0]--;
+                return true;
+            }
+            else
+                return false;
+        }
+        
+        if(carType == 2)
+        {
+            if(v[1] != 0)
+            {
+                v[1]--;
+                return true;
+            }
+            else
+                return false;
+        }
+        
+        if(carType == 3)
+        {
+            if(v[2] != 0)
+            {
+                v[2]--;
+                return true;
+            }
+            else
+                return false;
+        }
+        
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
