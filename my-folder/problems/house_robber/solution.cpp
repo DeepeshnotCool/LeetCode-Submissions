8db113061cbdda2c121aa117b5d0c7cc4a236675
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-03-26 18:16:56 +0000

    Sync LeetCode submission - House Robber (cpp)

--- my-folder/problems/house_robber/solution.cpp
+++ my-folder/problems/house_robber/solution.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
+    /*
     int helper(vector<int> &nums, int index, vector<int> &arr){
+        //cout<<index<<endl;
          if(index >= nums.size())
              return 0;
          if(nums.size() == 1)
@@ -11,7 +13,9 @@ public:
          }
          int a = nums[index] + helper(nums, index+2, arr);
          int b = helper(nums, index+1, arr);
+         cout<<index<<"-"<<max(a,b)<<" "<<a<<","<<b<<endl;
          arr[index] = max(a, b);
+        
          return arr[index];
      }
     int rob(vector<int>& nums) {
@@ -20,4 +24,27 @@ public:
         vector<int> arr(size+1, -1);
         return helper(nums, index, arr);        
     }
+    */
+    
+    int rob(vector<int>& nums) {
+        if(nums.size() < 2){
+            return nums[0];
+        }
+        vector<int> arr(nums.size()+1, 0);
+        arr[nums.size()-1] = nums[nums.size()-1];
+        arr[nums.size()-2] = max(nums[nums.size()-2], nums[nums.size()-1]);
+        for(int i = nums.size()-3; i >= 0; i--){
+            arr[i] = max(arr[i+1], nums[i] + arr[i+2]);
+        }
+        return arr[0];
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 };
