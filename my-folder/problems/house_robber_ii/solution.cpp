56d04efbb7cbe2ce89cc0d6be0b355a7dccb6bc3
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-03-27 22:24:35 +0000

    Sync LeetCode submission - House Robber II (cpp)

--- my-folder/problems/house_robber_ii/solution.cpp
+++ my-folder/problems/house_robber_ii/solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int helper(vector<int> &nums, int index, vector<int> &arr, int first){
          if(index >= nums.size())
              return 0;
@@ -19,6 +20,7 @@ public:
          // }
          int a = nums[index] + helper(nums, index+2, arr, first);
          int b = helper(nums, index+1, arr, first);
+         cout<<index<<"-"<<max(a,b)<<" "<<a<<","<<b<<endl;
          arr[index] = max(a, b);
          return arr[index];
      }
@@ -29,7 +31,26 @@ public:
         int ans1 = helper(nums, index, arr, 0);
         vector<int> arr1(size + 1, -1);
         int ans = max(ans1 , helper(nums, index + 1, arr1, 1));
-        cout<<ans1<<ans;
+        //cout<<ans1<<ans;
         return ans;
     }
+    */
+    
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1)
+            return nums[0];
+        if(nums.size() == 2)
+            return max(nums[0], nums[1]);
+        vector<int> memo(nums.size()+1, 0);
+        memo[nums.size()-2] = nums[nums.size()-2];
+        for(int i = nums.size()-3; i >= 0; i--){
+            memo[i] = max(memo[i+1], memo[i+2]+nums[i]);
+        }
+        memo[nums.size()-1] = nums[nums.size()-1];
+        for(int i = nums.size()-2; i >= 1; i--){
+            memo[i] = max(memo[i+1], memo[i+2]+nums[i]);
+        }
+        return max(memo[0],memo[1]);
+    }
+    
 };
