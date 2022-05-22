8ed7018abb41b3bac6329c0b5fdfe7a9bca4d739
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-05-22 02:40:53 +0000

    Sync LeetCode submission - Percentage of Letter in String (cpp)

---
+++ my-folder/problems/percentage_of_letter_in_string/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count = 0;
+        for(auto it: s){
+            if(it == letter)
+                count++;
+        }
+        cout<<count;
+        return floor((count * 100)/s.size());
+    }
+};
