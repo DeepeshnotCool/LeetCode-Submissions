ccf7df4af5ecb4c89c3678325ad74dbf993fb3ab
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-10-28 18:48:06 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- my-folder/problems/group_anagrams/solution.cpp
+++ my-folder/problems/group_anagrams/solution.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+
         map<string,vector<string>> m;
         for(int i = 0; i < strs.size(); i++)
         {
             string s = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            m[strs[i]].push_back(s);
+            //sort(strs[i].begin(), strs[i].end());
+            vector<int> alphacount(26,0);
+            for (auto it : s) {
+                alphacount[it - 'a']++;
+            }
+            string temp = "";
+            for (int j = 0; j < 26; j++) {
+                for(int k = 0; k < alphacount[j]; k++)
+                    temp += to_string(j + 'a');
+            }
+            m[temp].push_back(s);
         }
         
         vector<vector<string>> v;
@@ -15,8 +26,11 @@ public:
             v.push_back(it.second);
         }
         return v;
-        
-        
-        
+
+
+
+
+
+
     }
 };
