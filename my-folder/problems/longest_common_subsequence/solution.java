a088bb311a028c07b59a8723b02fb084c2a6ee67
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-12-19 12:56:13 +0000

    Sync LeetCode submission - Longest Common Subsequence (java)

--- my-folder/problems/longest_common_subsequence/solution.java
+++ my-folder/problems/longest_common_subsequence/solution.java
@@ -1,12 +1,40 @@
 class Solution {
-public int longestCommonSubsequence(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        if (m < n)  return longestCommonSubsequence(s2, s1);
-        int[][] dp = new int[2][n + 1];
-        for (int i = 0, k = 1; i < m; ++i, k ^= 1)
-            for (int j = 0; j < n; ++j)
-                if (s1.charAt(i) == s2.charAt(j)) dp[k][j + 1] = 1 + dp[k ^ 1][j];
-                else dp[k][j + 1] = Math.max(dp[k ^ 1][j + 1], dp[k][j]);
-        return dp[m % 2][n];
+   // public static int t[][];
+    public int longestCommonSubsequence(String text1, String text2) {
+       int n = text1.length();
+       int m = text2.length();
+
+       int[][]t = new int[n+1][m+1];
+       for(int i=0; i<n; i++)
+       {
+           for(int j=0; j<m; j++)
+           {   if(i==0|| j==0)
+                 t[i][j] = 0;
+           }
+       }
+       for(int i=1; i<=n; i++)
+       {
+        for(int j=1; j<=m; j++)
+        {
+             if(text1.charAt(i-1)== text2.charAt(j-1))
+                t[i][j] = 1+ t[i-1][j-1];
+             else
+               t[i][j] = Math.max(t[i-1][j], t[i][j-1]); 
+        }
+       }
+       return t[n][m];
     }
+
+    // public int lcs(String text1, String text2, int n, int m)
+    // {
+    //     if(n==0 || m==0)
+    //       return 0;
+    //     if(t[n][m] != -1)
+    //         return t[n][m];
+
+    //      if(text1.charAt(n-1) == text2.charAt(m-1))
+    //        return t[n][m] = 1 + lcs(text1, text2, n-1, m-1);
+    //      else  
+    //         return t[n][m] = Math.max(lcs(text1, text2, n-1,m), lcs(text1, text2, n,m-1));    
+    // } 
 }
