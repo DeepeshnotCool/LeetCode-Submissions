cccc8a627b74312f10ecf14ce8a21f389c4d95b4
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-12-27 06:00:43 +0000

    Sync LeetCode submission - Patients With a Condition (mysql)

--- my-folder/problems/patients_with_a_condition/solution.sql
+++ my-folder/problems/patients_with_a_condition/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT * FROM Patients WHERE conditions LIKE 'DIAB1%' OR  conditions LIKE '% DIAB1%';
+select * from Patients where conditions like '% DIAB1%' or conditions like 'DIAB1%'; 
