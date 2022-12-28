0de27c50e09d0f06d950fd821f8dbdfd6e4ba260
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2022-12-28 18:09:10 +0000

    Sync LeetCode submission - Employees With Missing Information (mysql)

--- my-folder/problems/employees_with_missing_information/solution.sql
+++ my-folder/problems/employees_with_missing_information/solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
- SELECT employee_id FROM Employees e WHERE employee_id NOT IN(SELECT employee_id FROM Salaries)
- UNION
- SELECT employee_id FROM Salaries s WHERE employee_id NOT IN(SELECT employee_id FROM Employees)
- ORDER BY employee_id;
-# SELECT Employees.employee_id FROM Employees CROSS JOIN Salaries WHERE salary = null OR name = null;
+select employee_id from employees where employee_id not in (select employee_id from salaries)
+union 
+select employee_id from salaries where employee_id not in (select employee_id from employees) 
+order by employee_id
