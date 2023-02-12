7f96d44986120a6009c85145262c4df7a9b22b7a
DeepeshnotCool <58002365+DeepeshnotCool@users.noreply.github.com>
2023-02-12 20:39:48 +0000

    Sync LeetCode submission - Minimum Fuel Cost to Report to the Capital (cpp)

--- my-folder/problems/minimum_fuel_cost_to_report_to_the_capital/solution.cpp
+++ my-folder/problems/minimum_fuel_cost_to_report_to_the_capital/solution.cpp
@@ -1,33 +1,31 @@
-const int maxn = 1e5+12;
-long long output;
 class Solution {
-public:
-    int st;
-    vector<vector<int>> list_adj;
-    vector<int> sub_tree;
-    void dfs(int root,int parameter)
-    {
-        sub_tree[root]=1;
-        for(auto x : list_adj[root])
-             if(x!=parameter)
-             {
-                 dfs(x,root);
-                 output += (sub_tree[x]+st-1)/st;
-                 sub_tree[root] += sub_tree[x];
-             }
+ public:
+  long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+    long long ans = 0;
+    vector<vector<int>> graph(roads.size() + 1);
+
+    for (const vector<int>& road : roads) {
+      const int u = road[0];
+      const int v = road[1];
+      graph[u].push_back(v);
+      graph[v].push_back(u);
     }
-    long long minimumFuelCost(vector<vector<int>>& e, int available_seats) {
-        int n=e.size()+1;
-        output=0;
-        st=available_seats;
-        list_adj.assign(n+1,vector<int>());
-        sub_tree.assign(n+1,0);
-        for(auto x : e) 
-        {
-            list_adj[x[0]].push_back(x[1]);
-            list_adj[x[1]].push_back(x[0]);
-        }
-        dfs(0,-1);
-        return output; 
+
+    dfs(graph, 0, -1, seats, ans);
+    return ans;
+  }
+
+ private:
+  int dfs(const vector<vector<int>>& graph, int u, int prev, int seats,
+          long long& ans) {
+    int people = 1;
+    for (const int v : graph[u]) {
+      if (v == prev)
+        continue;
+      people += dfs(graph, v, u, seats, ans);
     }
+    if (u > 0)
+      ans += (people + seats - 1) / seats;
+    return people;
+  }
 };
